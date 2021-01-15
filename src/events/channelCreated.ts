c890d1dff49424fd04e6e280040f8b94a5aac62e
yukiyukki <yuki.nagashima@stafes.com>
2021-01-15 10:16:53 +0900

    migrate events api

--- src/events/channelCreated.ts
+++ src/events/channelCreated.ts
@@ -4,43 +4,37 @@ import {
 } from "@slack/web-api";
 
 export const registerChannelCreated = (app: App) => {
-  app.event<"channel_created">(
-    "channel_created",
-    async ({ next, context, payload }) => {
-      if (payload.type !== "channel_created") {
-        next();
-        return;
-      }
+  app.event('channel_created', async ({ event }) => {
+    if (event.type !== "channel_created") {
+      return;
+    }
 
-      const channel = payload.channel;
+    const channel = event.channel;
 
-      const message = `新しいchannelが作成されました！\n#${channel.name}`;
+    const message = `新しいchannelが作成されました！\n#${channel.name}`;
 
-      const token = process.env.SLACK_BOT_TOKEN;
+    const token = process.env.SLACK_BOT_TOKEN;
 
-      const params: ChatPostMessageArguments = {
+    const params: ChatPostMessageArguments = {
+      token,
+      channel: "#z-feed-new-channel",
+      text: message,
+      link_names: true,
+    };
+    await app.client.chat.postMessage(params);
+
+    const channelNameRule = new RegExp(`^(${process.env.SLACK_CHANNEL_RULES})-`);
+    if (!channelNameRule.test(channel.name)) {
+      const alertParams: ChatPostMessageArguments = {
         token,
-        channel: "#z-feed-new-channel",
-        text: message,
-        link_names: true,
-      };
-      await app.client.chat.postMessage(params);
-
-      const channelNameRule = new RegExp(`^(${process.env.SLACK_CHANNEL_RULES})-`);
-      if (!channelNameRule.test(channel.name)) {
-        const alertParams: ChatPostMessageArguments = {
-          token,
-          channel: `#${channel.name}`,
-          text: `:female-police-officer:新しく作成された channel #${channel.name} はガイドライン外のchannel名です。
+        channel: `#${channel.name}`,
+        text: `:female-police-officer:新しく作成された channel #${channel.name} はガイドライン外のchannel名です。
 ガイドラインに適した名前に変更をお願いします:relaxed:
 <${process.env.SLACK_GUIDELINE_URL}|STAFES Slack Guideline>`,
-          link_names: true,
-          unfurl_links: true,
-        };
-        await app.client.chat.postMessage(alertParams);
-      }
-
-      next();
+        link_names: true,
+        unfurl_links: true,
+      };
+      await app.client.chat.postMessage(alertParams);
     }
-  );
+  });
 };
--- src/events/emojiChanged.ts
+++ src/events/emojiChanged.ts
@@ -5,39 +5,47 @@ import {
 } from "@slack/web-api";
 
 export const registerEmojiChanged = (app: App) => {
-  app.event<'emoji_changed'>(
-    'emoji_changed',
-    async ({ next, context, payload }) => {
-      if (payload.subtype !== 'add'
-        || !payload.name) {
-        next();
-        return;
-      }
+  app.event('emoji_changed', async ({ event, client, context }) => {
+    if (event.subtype !== 'add' || !event.name) {
+      return;
+    }
 
-      const message = `新しいemojiが追加されました: \`:${payload.name}:\``;
+    const token = process.env.SLACK_BOT_TOKEN;
 
-      const token = process.env.SLACK_BOT_TOKEN;
-
-      const imageUrl = payload.value;
-
-      let attachments: Array<MessageAttachment> = [];
-      if (imageUrl !== '') {
-        const attachment: MessageAttachment = {
-          fallback: payload.name,
-          image_url: imageUrl,
-        };
-        attachments = [attachment];
-      }
+    if (event.value?.startsWith('alias:')) {
+      const aliases = event.value.split(':');
+      const message = `\`:${aliases[1]}:\`に新しい別名が追加されました: \`:${event.name}:\``;
 
       const params: ChatPostMessageArguments = {
         token,
         channel: '#z-feed-new-emoji',
         text: message,
-        icon_emoji: `:${payload.name}:`,
-        attachments,
+        icon_emoji: `:${event.name}:`,
       };
       await app.client.chat.postMessage(params);
-      next();
+      return;
     }
-  );
+
+    const imageUrl = event.value;
+
+    let attachments: Array<MessageAttachment> = [];
+    if (imageUrl !== '') {
+      const attachment: MessageAttachment = {
+        fallback: event.name,
+        image_url: imageUrl,
+      };
+      attachments = [attachment];
+    }
+
+    const message = `新しいemojiが追加されました: \`:${event.name}:\``;
+
+    const params: ChatPostMessageArguments = {
+      token,
+      channel: '#z-feed-new-emoji',
+      text: message,
+      icon_emoji: `:${event.name}:`,
+      attachments,
+    };
+    await app.client.chat.postMessage(params);
+  });
 };
